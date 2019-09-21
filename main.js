a407762200423dd52da4d80a7b923b3a218c7d6c
jenk1 <19733552+jenk1@users.noreply.github.com>
2019-09-21 12:03:09 -0400

    Updated files upload

--- main.js
+++ main.js
@@ -3,14 +3,20 @@ const url = require('url');
 const path = require('path');
 
 // Get some stuff from electron
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function() {
   // create new new window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+      //The lines below solved the issue of require not defined
+      webPreferences: {
+        nodeIntegration: true
+      }
+  });
 
   // load the html file into the window
   mainWindow.loadURL(url.format({
@@ -18,11 +24,47 @@ app.on('ready', function() {
     protocol: 'file:',
     slashes: true
   }));
-
+  // quit when app closed
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
   // build the menu from the template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // insert the menu
   Menu.setApplicationMenu(mainMenu);
+});
+
+
+  // Handle create add window
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+      width: 300,
+      height: 200,
+      title: 'Adding Shopping List Item',
+
+      //The lines below solved the issue of require not defined
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    // load the html file into the window
+    addWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'addWindow.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+    // garbage collection handle
+    addWindow.on('close', function() {
+      addWindow = null;
+    })
+
+  }
+
+// catch item:add
+ipcMain.on('item:add', function(e, item) {
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
 });
 
 // create a menu template
@@ -31,11 +73,18 @@ const mainMenuTemplate = [
     label: 'File',
     submenu: [
       {
-        label: 'Add Item'
-        // stopped video on 17:17
+        label: 'Add Item',
+        accelerator: process.platform == 'darwin' ? 'command+M':
+        'Ctrl+M',
+        click() {
+          createAddWindow();
+        }
       },
       {
-        label: 'Clear Items'
+        label: 'Clear Items',
+        click() {
+            mainWindow.webContents.send('item:clear');
+        }
       },
       {
         label: 'Quit',
@@ -47,4 +96,29 @@ const mainMenuTemplate = [
       }
     ]
   }
-]
+];
+
+// If mac, add emtpy object to menu
+if(process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+
+// add developer tools if not in production
+if(process.env.NODE_ENV != 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'command+I':
+        'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
